6b51918ea325cf6ac731552a49a51ab12b430dfe
Ausha M S <anusgowda050@gmail.com>
2023-07-24 14:19:25 +0530

    change style of the error page.

--- ConsumerApp/src/components/CheckIn/ErrorPage.js
+++ ConsumerApp/src/components/CheckIn/ErrorPage.js
@@ -5,10 +5,10 @@ import { Icon } from "@mui/material";
 const Error = () => {
   return (
     <>
-      <div style={{ color: "red", textAlign: "center" }}>
+      <div style={{color: "red", textAlign: "center", 
+                    fontSize: "30px", display:"flex", 
+                    justifyContent:"center", alignItems:"center" }}>
        <p>Something went wrong. Please re-scan the QR code on the table to check-in</p>
-      </div>
-      <div>
       </div>
     </>
   );
